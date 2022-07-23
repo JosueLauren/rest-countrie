c416780bfd62efaa959ff6647ca7ccc1464bccdd
Josue Laurentino <josue.laurentino@csptecnologia.com>
2022-07-23 14:08:32 -0300

    criar filtros da página home

--- src/components/ContentHome/index.tsx
+++ src/components/ContentHome/index.tsx
@@ -1,22 +1,103 @@
+import { useEffect, useState } from "react";
 import { Container, ContainerFilters } from "./style";
 
+interface IinfoCountries {
+  flags: {
+    png: string;
+    svg: string;
+  };
+  name: {
+    common: string;
+  };
+  population: number;
+  region: string;
+  capital: string[];
+}
+
 export const ContentHome = () => {
+  const [selectFilter, setSelelctFilter] = useState("");
+  const [inputFilter, setInputFilter] = useState("");
+  const [countries, setCountries] = useState<IinfoCountries[] | null>(null);
+
+  useEffect(() => {
+    async function fetchCoutries() {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchCoutries();
+  }, []);
+
+  function filterCoutries() {
+    if (selectFilter.trim() === "" && inputFilter.trim() === "") {
+      return countries;
+    }
+
+    const coutriesFilterSelect = countries?.filter(
+      (country) => country.region === selectFilter
+    );
+
+    let countriesFiltered = null;
+
+    if (coutriesFilterSelect?.length !== 0) {
+      countriesFiltered = coutriesFilterSelect?.filter((country) =>
+        country.name.common.toLowerCase().includes(inputFilter)
+      );
+    } else {
+      countriesFiltered = countries?.filter((country) =>
+        country.name.common.toLowerCase().includes(inputFilter)
+      );
+    }
+
+    return countriesFiltered;
+  }
+
   return (
     <Container>
       <ContainerFilters>
-        <input type="text" placeholder="Search for a country..." />
-        <select name="" id="">
-          <option value="" disabled>
-            Filter by region
-          </option>
-          <option value="">África</option>
-          <option value="">Ásia</option>
-          <option value="">América</option>
-          <option value="">Europa</option>
-          <option value="">Oceania</option>
+        <input
+          type="text"
+          value={inputFilter}
+          placeholder="Search for a country..."
+          onChange={(e) => setInputFilter(e.target.value)}
+        />
+        <select onChange={(e) => setSelelctFilter(e.target.value)}>
+          <option value="">Filter by region</option>
+          <option value="Africa">África</option>
+          <option value="Asia">Ásia</option>
+          <option value="Americas">América</option>
+          <option value="Europe">Europa</option>
+          <option value="Oceania">Oceania</option>
         </select>
       </ContainerFilters>
-      <div></div>
+      <div className="container-coutries">
+        {filterCoutries()?.map((country) => {
+          return (
+            <div key={country.name.common}>
+              <div>
+                <img src={country.flags.svg} alt="bandeira de um país" />
+              </div>
+              <h3>{country.name.common}</h3>
+              <div>
+                <p>
+                  Population: <span>{country.population}</span>
+                </p>
+                <p>
+                  Region: <span>{country.region}</span>
+                </p>
+                <p>
+                  Capital: <span>{country.capital}</span>
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </Container>
   );
 };
--- src/components/ContentHome/style.ts
+++ src/components/ContentHome/style.ts
@@ -7,6 +7,56 @@ import iconSearch from "./../../assets/search.svg";
 export const Container = styled.main`
     min-height: 100vh;
     background-color: ${(props) => props.theme.background};
+
+    .container-coutries{
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+        grid-gap: 20px;
+        padding: 0 5rem;
+
+        > div {
+
+            background-color: ${(props) => props.theme.elements};
+            max-width: 280px;
+            width: 100%;
+            padding-bottom: 30px;
+            cursor: pointer;
+            border-radius: 5px;
+            transition: transform 0.3s linear;
+
+            &:hover {
+                transform: scale(1.1);
+            }
+            
+            img {
+                max-width: 100%;
+                object-fit: cover;
+                border-top-left-radius: 5px;
+                border-top-right-radius: 5px;
+            }
+
+            h3 {
+                color: ${(props) => props.theme.text};
+                margin: 10px auto;
+                font-size: 1.5rem;
+                padding: 0 20px;
+            }
+
+            p {
+                color: ${(props) => props.theme.text};
+                font-size: 1.1rem;
+                padding: 0 20px;
+                margin-bottom: 10px;
+
+                span {
+                    opacity: 0.8;
+                    font-size: 1rem;
+                }
+            }
+                
+        }
+
+    }
 `
 
 export const ContainerFilters = styled.div`
